a0e97254815fcabc42fa35a1c4320d2f3e9fc13f
Carlos Daniel <ruuufffaria@gmail.com>
2022-10-10 23:18:49 -0300

    followed channels data

---
+++ src/components/SideBar/data.js
@@ -0,0 +1,76 @@
+const data = [
+  {
+    id: 1,
+    user: "User 1",
+    game: "Minecraft",
+    viewers: 10900,
+  },
+  {
+    id: 2,
+    user: "User 2",
+    game: "Terraria",
+    viewers: 10300,
+  },
+  {
+    id: 3,
+    user: "User 3",
+    game: "VALORANT",
+    viewers: 3500,
+  },
+  {
+    id: 4,
+    user: "User 4",
+    game: "Overwatch 2",
+    viewers: 1800,
+  },
+  {
+    id: 5,
+    user: "User 5",
+    game: "League of Legends",
+    viewers: 1500,
+  },
+  {
+    id: 6,
+    user: "User 6",
+    game: "Teamfight Tactics",
+    viewers: 467,
+  },
+  {
+    id: 7,
+    user: "User 7",
+    game: "VALORANT",
+    viewers: 405,
+  },
+  {
+    id: 8,
+    user: "User 8",
+    game: "Terraria",
+    viewers: 279,
+  },
+  {
+    id: 9,
+    user: "User 9",
+    game: "Genshin Impact",
+    viewers: 231,
+  },
+  {
+    id: 10,
+    user: "User 10",
+    game: "VALORANT",
+    viewers: 230,
+  },
+  {
+    id: 11,
+    user: "User 11",
+    game: "Teamfight Tactics",
+    viewers: 228,
+  },
+  {
+    id: 12,
+    user: "User 12",
+    game: "League of Legends",
+    viewers: 174,
+  },
+];
+
+export default data;
