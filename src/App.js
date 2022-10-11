54276235070284d9aad5f7c4b101e50aa625f4d6
Carlos Daniel <ruuufffaria@gmail.com>
2022-10-11 18:02:36 -0300

    feat: system dark theme

--- src/App.js
+++ src/App.js
@@ -3,7 +3,20 @@ import SideBar from "./components/SideBar";
 
 function App() {
   const theme = localStorage.getItem("theme");
-  if (theme === "dark") document.documentElement.classList.add("dark");
+
+  if (window.matchMedia && theme === null) {
+    const systemTheme = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    ).matches;
+
+    if (systemTheme) {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    }
+  } else if (theme === "dark") {
+    document.documentElement.classList.add("dark");
+    localStorage.setItem("theme", "dark");
+  }
 
   return (
     <div className="min-h-screen bg-body-bg">
