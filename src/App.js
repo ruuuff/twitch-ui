d11cc26c8126103ed88056b07a7eb608831aff7c
Carlos Daniel <ruuufffaria@gmail.com>
2022-10-12 18:00:06 -0300

    main content container

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import NavBar from "./components/NavBar";
 import SideBar from "./components/SideBar";
+import MainContent from "./components/MainContent";
 
 function App() {
   const theme = localStorage.getItem("theme");
 
   if (window.matchMedia && theme === null) {
-    const systemTheme = window.matchMedia(
+    const isSystemDarkTheme = window.matchMedia(
       "(prefers-color-scheme: dark)"
     ).matches;
 
-    if (systemTheme) {
+    if (isSystemDarkTheme) {
       document.documentElement.classList.add("dark");
       localStorage.setItem("theme", "dark");
     }
@@ -21,8 +22,11 @@ function App() {
   return (
     <div className="min-h-screen bg-body-bg">
       <NavBar />
-      <div className="h-[calc(100vh-50px)]">
+      <div className="flex h-[calc(100vh-50px)]">
         <SideBar />
+        <div className="flex-1">
+          <MainContent />
+        </div>
       </div>
     </div>
   );
---
+++ src/components/MainContent/index.jsx
@@ -0,0 +1,10 @@
+function MainContent() {
+  return (
+    <main
+      className="w-full h-full scrollbar overflow-y-scroll mx-auto 
+      max-w-[2000px]"
+    ></main>
+  );
+}
+
+export default MainContent;
