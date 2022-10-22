4f32040920a6eba869a131dd86b3e1838900d971
Carlos Daniel <ruuufffaria@gmail.com>
2022-10-21 21:56:02 -0300

    fix layout styles

--- src/components/MainContent/index.jsx
+++ src/components/MainContent/index.jsx
@@ -2,15 +2,11 @@ import Carousel from "./Carousel";
 
 function MainContent() {
   return (
-    <main
-      className="w-full h-full scrollbar overflow-y-scroll mx-auto 
-      max-w-[2000px]"
-    >
-      <div
-        className="flex items-center h-[350px] px-[150px] py-[25px]
-        bg-carousel-bg"
-      >
-        <Carousel />
+    <main className="w-full h-full scrollbar overflow-y-scroll px-[30px]">
+      <div className="mx-auto max-w-[2000px]">
+        <div className="flex items-center h-[340px] py-[20px]">
+          <Carousel />
+        </div>
       </div>
     </main>
   );
