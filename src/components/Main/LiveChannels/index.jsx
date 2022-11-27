60dd76cd64cc93ca2a444b0beaac3106ea3eead8
Carlos Daniel <ruuufffaria@gmail.com>
2022-11-27 15:38:23 -0300

    get more items on click

--- src/components/Main/LiveChannels/index.jsx
+++ src/components/Main/LiveChannels/index.jsx
@@ -1,19 +1,21 @@
+import { useState } from "react";
 import useResponsiveGrid from "../../hooks/useResponsiveGrid";
 import { followedChannels } from "../../../data";
 import Channel from "./Channel";
 import ShowMore from "../ShowMore";
 
 export default function LiveChannels() {
-  const [data, style] = useResponsiveGrid(followedChannels);
+  const [showMore, setShowMore] = useState(false);
+  const [data, style] = useResponsiveGrid(followedChannels, showMore);
 
   return (
     <>
-      <div className="grid gap-[10px]" style={style}>
+      <div className="grid gap-[20px_10px]" style={style}>
         {data.map((channel, index) => (
           <Channel key={index} channel={channel} />
         ))}
       </div>
-      <ShowMore />
+      <ShowMore showMore={showMore} setShowMore={setShowMore} />
     </>
   );
 }
