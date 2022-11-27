f354487c608b8171cb946b95fcb7aa74f0cd073a
Carlos Daniel <ruuufffaria@gmail.com>
2022-11-27 15:35:54 -0300

    feat: category card

--- src/components/Main/Categories/index.jsx
+++ src/components/Main/Categories/index.jsx
@@ -1,16 +1,20 @@
 import useResponsiveGrid from "../../hooks/useResponsiveGrid";
 import { categories } from "../../../data";
+import CategoryCard from "./CategoryCard";
 
 export default function Categories() {
-  const [data, style] = useResponsiveGrid(categories, [12, 10, 8, 6, 4], 2);
+  const [data, style] = useResponsiveGrid(
+    categories,
+    false,
+    [12, 10, 8, 6, 4],
+    2
+  );
 
   return (
     <>
-      <div className="grid gap-[10px]" style={style}>
-        {data.map(({ category }, index) => (
-          <div key={index} className="bg-red-400">
-            {category}
-          </div>
+      <div className="grid gap-[10px] mt-[-1px]" style={style}>
+        {data.map((category, index) => (
+          <CategoryCard key={index} category={category} />
         ))}
       </div>
     </>
