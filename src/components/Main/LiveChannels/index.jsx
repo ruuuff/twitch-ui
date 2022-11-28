0f858267a00fc9900e8df234aa4596282c33872d
Carlos Daniel <ruuufffaria@gmail.com>
2022-11-28 18:09:56 -0300

    showing more items outside hook useResponsiveGrid

--- src/components/Main/LiveChannels/index.jsx
+++ src/components/Main/LiveChannels/index.jsx
@@ -6,14 +6,16 @@ import ShowMore from "../ShowMore";
 
 export default function LiveChannels() {
   const [showMore, setShowMore] = useState(false);
-  const [data, style] = useResponsiveGrid(followedChannels, showMore);
+  const { cols, style } = useResponsiveGrid([6, 5, 4, 3, 2]);
 
   return (
     <>
       <div className="grid gap-[20px_10px]" style={style}>
-        {data.map((channel, index) => (
-          <Channel key={index} channel={channel} />
-        ))}
+        {followedChannels
+          .slice(0, showMore ? cols * 2 : cols)
+          .map((channel, index) => (
+            <Channel key={index} channel={channel} />
+          ))}
       </div>
       <ShowMore showMore={showMore} setShowMore={setShowMore} />
     </>
