a5562afe5e565d8c140db77c84f5bf950e037467
Carlos Daniel <ruuufffaria@gmail.com>
2022-12-01 18:52:40 -0300

    remove user select

--- src/components/LiveTag.jsx
+++ src/components/LiveTag.jsx
@@ -2,7 +2,7 @@ export default function LiveTag({ top = "10px", left = "10px" }) {
   return (
     <div
       className={`absolute font-inter text-[13px] bg-live-bg-color 
-      font-semibold text-white rounded-[4px] px-[5px] 
+      font-semibold text-white rounded-[4px] px-[5px] select-none
       top-[${top}] left-[${left}]`}
     >
       LIVE
