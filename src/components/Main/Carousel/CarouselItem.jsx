b1e4129a4b65fb164d4b7eda9148c0dd1fffb5ca
Carlos Daniel <ruuufffaria@gmail.com>
2022-12-04 17:41:48 -0300

    refactor: carousel item container component

--- src/components/Main/Carousel/CarouselItem.jsx
+++ src/components/Main/Carousel/CarouselItem.jsx
@@ -1,6 +1,7 @@
 import { getViewersFormatted } from "../../../utils";
 import LiveTag from "../../LiveTag";
 import Marker from "../../Marker";
+import CarouselItemContainer from "./CarouselItemContainer";
 
 function CarouselItem({
   index,
@@ -10,109 +11,101 @@ function CarouselItem({
   firstInteraction,
   manualUpdateCenterItem,
 }) {
-  const { user, game, viewers, tags, avatar } = channel;
+  const {
+    user,
+    game,
+    viewers,
+    tags,
+    avatar,
+    imagePreview = "/assets/404_preview.jpg",
+  } = channel;
   const isCentral = index === centralItem;
 
-  function getStyle() {
-    // center
-    if (isCentral) return "w-[750px] h-[300px] left-[50%] z-[8]";
-    const itemIndex = showItems.indexOf(index);
-
-    // 2
-    if (itemIndex === 2 || itemIndex === showItems.length - 3) {
-      return (
-        "w-[637px] h-[255px] z-[7] " +
-        (itemIndex === 2 ? "left-[40%]" : "left-[60%]")
-      );
-    }
-
-    // 3
-    if (itemIndex === 1 || itemIndex === showItems.length - 2) {
-      return (
-        "w-[525px] h-[210px] z-[6] " +
-        (itemIndex === 1 ? "left-[30%]" : "left-[70%]")
-      );
-    }
-
-    // hidden
-    if (itemIndex === 0 || itemIndex === showItems.length - 1) {
-      return (
-        "w-[400px] h-[169px] z-[5] opacity-0 pointer-events-none " +
-        (itemIndex === 0 ? "left-[20%]" : "left-[80%]")
-      );
-    }
-
-    return "h-[0px] w-[0px] left-[50%] opacity-0";
-  }
-
   return (
-    <div
-      className={`absolute top-[50%] shadow-lg transform translate-x-[-50%] 
-      translate-y-[-50%] ${!isCentral && "hover:scale-[1.03]"} ${getStyle()}`}
-      style={{
-        transition:
-          "left .6s, width .6s, height .6s, opacity .6s, transform .2s",
-      }}
-      onClick={manualUpdateCenterItem}
+    <CarouselItemContainer
+      index={index}
+      isCentral={isCentral}
+      showItems={showItems}
+      manualUpdateCenterItem={manualUpdateCenterItem}
     >
-      <div
-        className={`relative h-full bg-black cursor-pointer ${
-          isCentral
-            ? "w-[530px] transition-[width] duration-[0s] delay-1000"
-            : "w-full"
-        }`}
-      >
-        {isCentral && <LiveTag />}
+      <div className="absolute inset-0">
+        <div
+          className="absolute inset-0 bg-[rgba(0,0,0,0.4)] 
+          group-hover:bg-transparent transition-all duration-200"
+        ></div>
+        <img
+          className="h-full w-full object-cover"
+          src={imagePreview}
+          alt="Live Preview"
+        />
       </div>
 
       {isCentral && (
         <div
-          className={`absolute right-0 top-0 bottom-0 w-[220px]
-          bg-carousel-item-bg ${
+          className={`absolute inset-0 flex h-full ${
             firstInteraction && "opacity-0 animate-fadeIn"
-          }`}
+          }
+        `}
         >
-          <div className="w-full h-full p-[10px] space-y-[10px]">
-            <div className="flex">
-              <div>
-                <img
-                  className="w-[50px] h-50px rounded-full"
-                  src={avatar}
-                  alt={user}
-                />
-              </div>
-              <div className="ml-[10px] text-[13px]">
-                <a
-                  href="/#"
-                  className="block text-purple font-inter font-semibold hover:underline"
-                >
-                  {user}
-                </a>
-                <a
-                  href="/#"
-                  className="block text-purple font-inter hover:underline"
-                >
-                  {game}
-                </a>
-                <span className="block font-inter text-carousel-text-color">
-                  {getViewersFormatted(viewers)} viewers
-                </span>
-              </div>
-            </div>
+          {/* "player" */}
+          <div
+            className="relative h-full bg-black cursor-pointer w-[530px] 
+            transition-[width] duration-[0s] delay-[700ms]"
+          >
+            <LiveTag />
 
-            <div className="flex">
-              {tags.map((tag, index) => (
-                <Marker key={index} tag={tag} />
-              ))}
-            </div>
+            {/* <img
+              className="h-full w-full object-cover"
+              src={imagePreview}
+              alt="Live Preview"
+            /> */}
+          </div>
 
-            <p className="font-inter text-[13px] text-carousel-text-color">
-              {user} is live! Come hang out in chat.
-            </p>
+          {/* channel infos */}
+          <div className="w-[220px] bg-carousel-item-bg">
+            <div className="w-full h-full p-[10px] space-y-[10px]">
+              <div className="flex">
+                <div>
+                  <img
+                    className="w-[50px] h-50px rounded-full"
+                    src={avatar}
+                    alt={user}
+                  />
+                </div>
+
+                <div className="ml-[10px] text-[13px]">
+                  <a
+                    href="/#"
+                    className="block text-purple font-inter font-semibold hover:underline"
+                  >
+                    {user}
+                  </a>
+                  <a
+                    href="/#"
+                    className="block text-purple font-inter hover:underline"
+                  >
+                    {game}
+                  </a>
+                  <span className="block font-inter text-carousel-text-color">
+                    {getViewersFormatted(viewers)} viewers
+                  </span>
+                </div>
+              </div>
+
+              <div className="flex">
+                {tags.map((tag, index) => (
+                  <Marker key={index} tag={tag} />
+                ))}
+              </div>
+
+              <p className="font-inter text-[13px] text-carousel-text-color">
+                {user} is live! Come hang out in chat.
+              </p>
+            </div>
           </div>
         </div>
       )}
-    </div>
+    </CarouselItemContainer>
   );
 }
 
