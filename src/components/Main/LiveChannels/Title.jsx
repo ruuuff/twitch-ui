cd14c0787ebd654925ce893cdc078c6afa51ed20
Carlos Daniel <ruuufffaria@gmail.com>
2022-12-05 14:42:00 -0300

    feat: show title entire on hover

--- src/components/Main/LiveChannels/Title.jsx
+++ src/components/Main/LiveChannels/Title.jsx
@@ -2,6 +2,7 @@ export default function Title({ title, bold }) {
   return (
     <div className="mb-[2px]">
       <a
+        title={title}
         className={`block mt-[-1px] text-[14px] text-livechannel-title-color
         hover:text-purple truncate ${bold ? "font-bold" : "font-semibold"}`}
         href="/#"
